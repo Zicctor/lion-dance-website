e9746769a6cd627782db2ddf05f1d7d6bb106916
zicxtorthuhai <svshirunai123@gmail.com>
2025-05-20 13:13:00 +0700

    temporary remove sponsor section until it needed

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,6 @@ export default function Home() {
   return (
     <div className="w-full overflow-hidden">
       <HeroSection />
-      <SponsorsSection />
       <AboutSection />
       <TestimonialsSection />
       <GallerySection />
